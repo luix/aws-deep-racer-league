110c19f611cb3720bc05777654ccc95e6aeec7d0
LuiX <luis.a.vivero@gmail.com>
2019-07-23 21:06:41 -0700

    Example of using waypoints and heading to make the car run in the right direction

--- ClosestWaypoints.py
+++ ClosestWaypoints.py
@@ -1,1 +1,7 @@
 def reward_function(params):
+
+    #################################################################################
+    '''
+    Example of using waypoints and heading to make the car run in the right direction
+    '''
+    
