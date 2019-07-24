88dce046896c42d6cc7852eb4d7d428121510233
LuiX <luis.a.vivero@gmail.com>
2019-07-23 21:08:33 -0700

    Read input variables

--- ClosestWaypoints.py
+++ ClosestWaypoints.py
@@ -4,4 +4,10 @@ def reward_function(params):
     '''
     Example of using waypoints and heading to make the car run in the right direction
     '''
-    
+
+    import math
+
+    # Read input variables
+    waypoints = params['waypoints']
+    closest_waypoints = params['closest_waypoints']
+    heading = params['heading']
