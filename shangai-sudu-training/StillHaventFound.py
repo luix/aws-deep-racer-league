a6293b1a3fd31e33261e1690f5f275f7f6cfded4
LuiX <luis.a.vivero@gmail.com>
2019-08-01 22:31:13 -0700

    Reward function for the Shanghai Sudu training

---
+++ shangai-sudu-training/StillHaventFound.py
@@ -0,0 +1,41 @@
+def reward_function(params):
+
+    #################################################################################
+    '''
+    Example of using waypoints and heading to make the car run in the right direction
+    '''
+
+    import math
+
+    # Read input variables
+    waypoints = params['waypoints']
+    closest_waypoints = params['closest_waypoints']
+    heading = params['heading']
+    speed = params['speed']
+
+    # Initialize the reward with typical value
+    reward = 1.0
+
+    # Calculate the direction of the center line based on the closest waypoints
+    next_point = waypoints[closest_waypoints[1]]
+    prev_point = waypoints[closest_waypoints[0]]
+
+    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
+    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
+
+    # Convert to degree
+    track_direction = math.degrees(track_direction)
+
+    # Calculate the difference between the track direction and the heading direction of the car
+    direction_diff = abs(track_direction - heading)
+
+    # Penalize the reward if the difference is too large
+    DIRECTION_THRESHOLD = 10.0
+    if direction_diff > DIRECTION_THRESHOLD:
+        reward *= 0.5
+
+    if reward == 1.0 and speed >= 1.75 and speed <= 4.0:
+        reward += speed
+
+    # Return reward as a float value
+    return float(reward)
