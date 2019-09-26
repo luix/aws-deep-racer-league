b6860ab56c028891196c11fef8851e5b969500c8
LuiX <luis.a.vivero@gmail.com>
2019-09-25 22:04:42 -0700

    Utility to plot track waypoints

---
+++ plotWaypoints.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Track Name from Tracks List
+track_name = "New_York_Track"
+
+# Location of tracks folder
+absolute_path = "."
+
+# Get waypoints from numpy file
+waypoints = np.load("%s/tracks/%s.npy" % (absolute_path, track_name))
+
+# Get number of waypoints
+print("Number of waypoints = " + str(waypoints.shape[0]))
+
+# Plot waypoints
+for i, point in enumerate(waypoints):
+    waypoint = (point[2], point[3])
+    plt.scatter(waypoint[0], waypoint[1])
+    print("Waypoint " + str(i) + ": " + str(waypoint))
+
+plt.show
