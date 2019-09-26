8c7269b712a58d7e579987c2f2002fb99fb98d12
LuiX <luis.a.vivero@gmail.com>
2019-09-25 22:15:18 -0700

    Plot Cumulo Carrera track

--- plotWaypoints.py
+++ plotWaypoints.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Track Name from Tracks List
-track_name = "New_York_Track"
+# track_name = "New_York_Track"
+track_name = "Mexico_track"
 
 # Location of tracks folder
 absolute_path = "."
 
 # Get waypoints from numpy file
-waypoints = np.load("%s/tracks/%s.npy" % (absolute_path, track_name))
+waypoints = np.load("%s/tracks-npy/%s.npy" % (absolute_path, track_name))
 
 # Get number of waypoints
 print("Number of waypoints = " + str(waypoints.shape[0]))
