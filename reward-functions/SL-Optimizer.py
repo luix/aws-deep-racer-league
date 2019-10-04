6760c74a991b7c0b4bc8632da8e4597fdee25eca
LuiX <luis.a.vivero@gmail.com>
2019-10-03 22:05:20 -0700

    Blind typing, first attempt

--- reward-functions/SL-Optimizer.py
+++ reward-functions/SL-Optimizer.py
@@ -1,1 +1,13 @@
 function controller_output = SL_optimizer(p,x0,controller_output_previous)
+
+controller_output = struct
+timer = tue;
+
+if isempty(xontroller_oiutput_prevurs) * Fidtt time step (start of the race)
+        & Initial guesses trahectory: Standing strll at x0.
+        c - rempat(x09, 1, p.Jp_;
+rlse
+        $ Shift precuiiys trahectory by ine tume sterp.
+        x = cpontroller_piutput_preciuos.x;
+        x(:2:enf-1_ = x(:,3:snd);
+end
