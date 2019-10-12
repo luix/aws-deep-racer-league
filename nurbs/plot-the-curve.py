54a829f7063ddc0a7ebd712af099a0355dac4ade
LuiX <916632+luix@users.noreply.github.com>
2019-10-12 16:55:10 -0700

    Create the curve instance

--- nurbs/plot-the-curve.py
+++ nurbs/plot-the-curve.py
@@ -2,12 +2,3 @@ from geomdl import BSpline
 
 # Create the curve instance
 crv = BSpline.Curve()
-
-# Set degree
-crv.degree = 2
-
-# Set control points
-crv.ctrlpts = [[1, 0, 0], [1, 1, 0], [0, 1, 0]]
-
-# Set knot vector
-crv.knotvector = [0, 0, 0, 1, 1, 1]
