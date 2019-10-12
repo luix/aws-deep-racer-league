94e8c9dea8b3bd70b59c544d3729ca7f3429a64b
LuiX <916632+luix@users.noreply.github.com>
2019-10-12 15:23:29 -0700

    Plot the sine of each point between 0 and 20 using matplotlib and numpy libraries

---
+++ standardplot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+msg = "Hello Guey"
+print(msg)
+
+x = np.linspace(0, 20, 100)     # Create a list of evenly-spaced numbers over the range
+plt.plot(x, np.sin(x))          # Plot the sine of each x point
+plt.show()                      # Display the plot
