3cfff6d3460bf8f405264391cd4561523bbded84
LuiX <916632+luix@users.noreply.github.com>
2019-10-12 17:43:33 -0700

    Plotting multiple curves

--- nurbs/plot-the-curve.py
+++ nurbs/plot-the-curve.py
@@ -1,26 +1,43 @@
 from geomdl import BSpline
+from geomdl import multi
 from geomdl import knotvector
 
-# Create the curve instance
-crv = BSpline.Curve()
+# Create the curve instance #1
+crv1 = BSpline.Curve()
 
 # Set degree
-crv.degree = 2
+crv1.degree = 2
 
 # Set control points
-crv.ctrlpts = [[1, 0, 0], [1, 1, 0], [0, 1, 0]]
+crv1.ctrlpts = [[1, 0, 0], [1, 1, 0], [0, 1, 0]]
 
 # Generate a uniform knot vector
-crv.knotvector = knotvector.generate(crv.degree, crv.ctrlpts_size)
+crv1.knotvector = knotvector.generate(crv1.degree, crv1.ctrlpts_size)
+
+# Create the curve instance #2
+crv2 = BSpline.Curve()
+
+# Set degree 
+crv2.degree = 3
+
+# Set control points
+crv2.ctrlpts = [[1, 0, 0], [1, 1, 0], [2, 1, 0], [1, 1, 0]]
+
+# Generate a uniform knot vector
+crv2.knotvector = knotvector.generate(crv2.degree, crv2.ctrlpts_size)
+
+# Create a curve container
+mcrv = multi.CurveContainer(crv1, crv2)
 
 # Import Matplotlib visualization module
 from geomdl.visualization import VisMPL
 
-# Set the visualization component of the curve
-crv.vis = VisMPL.VisCurve3D()
+# Set the visualization component of the curve container
+mcrv.vis = VisMPL.VisCurve3D()
 
-# Plot the curve
-crv.render()
+# Plot the curves in the curve container
+mcrv.render()
+
 
 '''
     Convert non-rational to rational curve
