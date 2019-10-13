266b30de299a6a6c691452050817570a9aba8021
LuiX <916632+luix@users.noreply.github.com>
2019-10-12 17:27:13 -0700

    Convert non-rational to rational curve

--- nurbs/plot-the-curve.py
+++ nurbs/plot-the-curve.py
@@ -16,7 +16,18 @@ crv.knotvector = [0, 0, 0, 1, 1, 1]
 from geomdl.visualization import VisMPL
 
 # Set the visualization component of the curve
-crv.vis = VisMPL.VisCurve3D
+crv.vis = VisMPL.VisCurve3D()
 
 # Plot the curve
 crv.render()
+
+'''
+    Convert non-rational to rational curve
+    
+    Generates a B-Spline (non-rational) curve and converts it 
+    into a NURBS (rational) curve.
+'''
+from geomdl import convert      # Import convert module
+
+# B-Spline to NURBS
+crv_rat = convert.bspline_to_nurbs(crv)
