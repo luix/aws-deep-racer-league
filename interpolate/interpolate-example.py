9e7824ea63bc36a609c7b63762202a6388fa1827
LuiX <luis.a.vivero@gmail.com>
2019-10-13 19:54:00 -0700

    Add 'Interpolation (scipy.interpolate)' link to articles

---
+++ interpolate/interpolate-example.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+from scipy.interpolate import interp1d
+
+x = np.linspace(0, 10, num=11, endpoint=True)
+y = np.cos(-x**2/9.0)
+f = interp1d(x, y)
+f2 = interp1d(x, y, kind='cubic')
+
+xnew = np.linspace(0, 10, num=41, endpoint=True)
+
+plt.plot(x, y, 'o', xnew, f(xnew), '-', xnew, f2(xnew), '--')
+plt.legend(['data', 'linear', 'cubic'], loc='best')
+plt.show()
