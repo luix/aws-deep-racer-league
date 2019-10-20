83a9863d22816c7cb4de07faec300f72ba26c84a
LuiX <916632+luix@users.noreply.github.com>
2019-10-20 15:38:52 -0700

    Use the obtained interpolation function and plot the result

--- interpolate/interpolate-example.py
+++ interpolate/interpolate-example.py
@@ -21,3 +21,10 @@ y = np.arange(-5.01, 5.01, 0.25)
 xx, yy = np.meshgrid(x, y)
 z = np.sin(xx**2+yy**2)
 f = interpolate.interp2d(x, y, z, kind='cubic')
+
+# Use the obtained interpolation function and plot the result
+xnew = np.arange(-5.01, 5.01, 1e-2)
+ynew = np.arange(-5.01, 5.01, 1e-2)
+znew = f(xnew, ynew)
+plt.plot(x, z[0, :], 'ro-', xnew, znew[0, :], 'b-')
+plt.show()
