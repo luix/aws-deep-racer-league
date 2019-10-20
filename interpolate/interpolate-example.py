df92d6ed263f7e7bce2a5e241937bf990bf0a853
LuiX <916632+luix@users.noreply.github.com>
2019-10-20 15:38:03 -0700

    Construct a 2-D grid and interpolate on it

--- interpolate/interpolate-example.py
+++ interpolate/interpolate-example.py
@@ -1,14 +1,23 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
-from scipy.interpolate import interp1d
+# from scipy.interpolate import interp1d
 
-x = np.linspace(0, 10, num=11, endpoint=True)
-y = np.cos(-x**2/9.0)
-f = interp1d(x, y)
-f2 = interp1d(x, y, kind='cubic')
+# x = np.linspace(0, 10, num=11, endpoint=True)
+# y = np.cos(-x**2/9.0)
+# f = interp1d(x, y)
+# f2 = interp1d(x, y, kind='cubic')
 
-xnew = np.linspace(0, 10, num=41, endpoint=True)
+# xnew = np.linspace(0, 10, num=41, endpoint=True)
 
-plt.plot(x, y, 'o', xnew, f(xnew), '-', xnew, f2(xnew), '--')
-plt.legend(['data', 'linear', 'cubic'], loc='best')
-plt.show()
+# plt.plot(x, y, 'o', xnew, f(xnew), '-', xnew, f2(xnew), '--')
+# plt.legend(['data', 'linear', 'cubic'], loc='best')
+# plt.show()
+
+
+# Construct a 2-D grid and interpolate on it
+from scipy import interpolate
+x = np.arange(-5.01, 5.01, 0.25)
+y = np.arange(-5.01, 5.01, 0.25)
+xx, yy = np.meshgrid(x, y)
+z = np.sin(xx**2+yy**2)
+f = interpolate.interp2d(x, y, z, kind='cubic')
