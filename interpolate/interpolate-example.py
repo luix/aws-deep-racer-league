f7168fd9845aa84fd97312fd3c9d4501226dc736
LuiX <916632+luix@users.noreply.github.com>
2019-10-20 16:11:38 -0700

    Parametric spline example

--- interpolate/interpolate-example.py
+++ interpolate/interpolate-example.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
-# from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d
 
-# x = np.linspace(0, 10, num=11, endpoint=True)
-# y = np.cos(-x**2/9.0)
-# f = interp1d(x, y)
-# f2 = interp1d(x, y, kind='cubic')
+x = np.linspace(0, 10, num=11, endpoint=True)
+y = np.cos(-x**2/9.0)
+f = interp1d(x, y)
+f2 = interp1d(x, y, kind='cubic')
 
-# xnew = np.linspace(0, 10, num=41, endpoint=True)
+xnew = np.linspace(0, 10, num=41, endpoint=True)
 
-# plt.plot(x, y, 'o', xnew, f(xnew), '-', xnew, f2(xnew), '--')
-# plt.legend(['data', 'linear', 'cubic'], loc='best')
-# plt.show()
+plt.plot(x, y, 'o', xnew, f(xnew), '-', xnew, f2(xnew), '--')
+plt.legend(['data', 'linear', 'cubic'], loc='best')
+plt.show()
 
 
-# Construct a 2-D grid and interpolate on it
+Construct a 2-D grid and interpolate on it
 from scipy import interpolate
 x = np.arange(-5.01, 5.01, 0.25)
 y = np.arange(-5.01, 5.01, 0.25)
@@ -27,4 +27,21 @@ xnew = np.arange(-5.01, 5.01, 1e-2)
 ynew = np.arange(-5.01, 5.01, 1e-2)
 znew = f(xnew, ynew)
 plt.plot(x, z[0, :], 'ro-', xnew, znew[0, :], 'b-')
+plt.show()
+
+# https://docs.scipy.org/doc/scipy/reference/tutorial/interpolate.html
+
+# Parametric spline
+from scipy import interpolate
+t = np.arange(0, 1.1, .1)
+x = np.sin(2*np.pi*t)
+y = np.cos(2*np.pi*t)
+tck, u = interpolate.splprep([x, y], s=0)
+unew = np.arange(0, 1.01, 0.01)
+out = interpolate.splev(unew, tck)
+plt.figure()
+plt.plot(x, y, 'x', out[0], out[1], np.sin(2*np.pi*unew), np.cos(2*np.pi*unew), x, y, 'b')
+plt.legend(['Linear', 'Cubic Spline', 'True'])
+plt.axis([-1.05, 1.05, -1.05, 1.05])
+plt.title('Spline of parametrically-defined curve')
 plt.show()
