cbf2c2767299257d7a3c2918f6b46fac1c255694
LuiX <916632+luix@users.noreply.github.com>
2019-10-19 18:06:55 -0700

    Fix Git commiter

--- git-filter-branch.sh
+++ git-filter-branch.sh
@@ -3,3 +3,8 @@ git filter-branch --env-filter 'if [ "$GIT_AUTHOR_EMAIL" = "luis.vivero@slickdea
      GIT_AUTHOR_NAME="LuiX";
      GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL;
      GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"; fi' -- --all
+
+
+
+
+git filter-branch -f --env-filter "GIT_AUTHOR_NAME='LuiX'; GIT_AUTHOR_EMAIL='luis.vivero@slickdeals.net'; GIT_COMMITTER_NAME='LuiX'; GIT_COMMITTER_EMAIL='916632+luix@users.noreply.github.com';" HEAD
